bc42692395dfe102c2ae1dc85ea1ab2a572dd8e0
tryada <dzadzam@gmail.com>
2024-05-13 15:25:58 +0200

    nawigacja, bootstrap, routing

---
+++ frontend/JobMasterClient/src/app/advertisements/advertisements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisements',
+  standalone: true,
+  imports: [],
+  templateUrl: './advertisements.component.html',
+  styleUrl: './advertisements.component.css'
+})
+export class AdvertisementsComponent {
+
+}
--- frontend/JobMasterClient/src/app/app.routes.ts
+++ frontend/JobMasterClient/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AdvertisementsComponent } from './advertisements/advertisements.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'advertisements', component: AdvertisementsComponent },
+    { path: 'profile', component: ProfileComponent },
+];
