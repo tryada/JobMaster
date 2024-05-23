304c40513adc82a7f8c0faf4e918816079deaa54
tryada <dzadzam@gmail.com>
2024-05-23 20:16:28 +0200

    tworzenie instancji identyfikatorów za pomocą metod

--- backend/src/JobMaster.Api/Advertisements/Mapping/AdvertisementMappingConfig.cs
+++ backend/src/JobMaster.Api/Advertisements/Mapping/AdvertisementMappingConfig.cs
@@ -5,6 +5,7 @@ using JobMaster.Application.Advertisements.Queries.GetAdvertisement;
 using JobMaster.Contracts.Advertisements;
 using JobMaster.Domain.Advertisements;
 using JobMaster.Domain.Advertisements.ValueObjects;
+using JobMaster.Domain.Skills.ValueObjects;
 using Mapster;
 
 namespace JobMaster.Api.Advertisements.Mapping;
@@ -14,21 +15,21 @@ public class AdvertisementMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.ForType<Advertisement, AdvertisementResponse>()
-            .Map(dest => dest.Id, src => src.Id.ToString())
+            .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.Skills, src => src.Skills.Select(x => x.ToString()).ToArray());
 
         config.ForType<CreateAdvertisementRequest, CreateAdvertisementCommand>()
-            .Map(dest => dest.Skills, src => src.Skills.Select(x => new Guid(x)).ToList());
+            .Map(dest => dest.Skills, src => src.Skills.Select(SkillId.Create).ToList());
 
         config.ForType<(UpdateAdvertisementRequest request, string id), UpdateAdvertisementCommand>()
-            .Map(dest => dest.Id, src => new Guid(src.id))
-            .Map(dest => dest.Skills, src => src.request.Skills.Select(x => new Guid(x)).ToList())
+            .Map(dest => dest.Id, src => AdvertisementId.Create(src.id))
+            .Map(dest => dest.Skills, src => src.request.Skills.Select(SkillId.Create).ToList())
             .Map(dest => dest, src => src.request);
 
         config.ForType<string, DeleteAdvertisementCommand>()
-            .MapWith(src => new DeleteAdvertisementCommand((AdvertisementId)new Guid(src)));
+            .MapWith(src => new DeleteAdvertisementCommand(AdvertisementId.Create(src)));
 
         config.ForType<string, GetAdvertisementQuery>()
-            .MapWith(src => new GetAdvertisementQuery((AdvertisementId)new Guid(src)));
+            .MapWith(src => new GetAdvertisementQuery(AdvertisementId.Create(src)));
     }
 }
